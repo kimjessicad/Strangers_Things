09033fb9c95b1ea8f077a7c5d83bd5dec259f8f4
Jessica <kimjessicad@gmail.com>
2022-07-06 10:13:37 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from 
+
+const root = ReactDOM.createRoot(document.getElementById('app'))
+root.render(<h1> Hello, world!</h1>)
