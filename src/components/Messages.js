3f79b207a29f01dd40a59f61724514d53f6c00d8
Eric Loving <ericdloving@gmail.com>
2022-07-11 08:33:18 -0500

    start of class monday

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,26 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useRef } from "react";
 
 
 const Messages = ({myMessages, myUsername}) => {
     const [messageFilter,setMessageFilter]=useState("sent")
+    const sentButton = useRef();
+    const receivedButton = useRef();
     let receivedMessages = [], sentMessages=[];
     receivedMessages = myMessages.filter((message)=>(message.fromUser.username != myUsername))
     sentMessages = myMessages.filter((message)=>(message.fromUser.username === myUsername))
     
     function handleInboxButtons(event) {
-        const filterSetting = event.target.value();
+        const filterSetting = (event.target.value)
         setMessageFilter(filterSetting);
+        console.log(filterSetting,messageFilter)
+        sentButton.current.checked=(filterSetting==="sent");
+        receivedButton.current.checked=(filterSetting==="received");
+        console.log(receivedMessages, sentMessages)
     }
 
     return (
          <div className="profileMessages">
              <h3 className="profileSectionTitle">Messages</h3>
              <div className = "inboxButtons" onChange={handleInboxButtons}>
-                 <input type="radio" value="sent" />
-                 <input type="radio" value="received" />
+                 <input ref={sentButton} type="radio" value="sent" />
+                 <input ref={receivedButton} type="radio" value="received" />
              </div>
-         {myMessages.length
-         ? myMessages.map((message) => {
+         {receivedMessages.length
+         ? receivedMessages.map((message) => {
              return (
                  <div key={message._id} className="message">
                  <h4>From: {message.fromUser.username}</h4>
